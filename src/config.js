2c7316bc5a488dbb0f6361b9c2b970f004b7b30b
Unknown <dimitar.petrov.311@gmail.com>
2018-02-22 16:04:48 +0200

    open weather key

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = (function configFunc() {
+    return {
+        currentWeatherApi: "http://api.openweathermap.org/data/2.5/weather?q=",
+        forecastWeatherApi: "http://api.openweathermap.org/data/2.5/forecast?q=",
+        key: "&APPID=50e4da3905ce0b0aecd4aa9da5e3ea71"
+    }
+})();
