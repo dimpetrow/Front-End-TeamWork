4d2dfea508e91ec6b4088eaafca8f18c0b712c04
Unknown <dimitar.petrov.311@gmail.com>
2018-02-25 17:21:27 +0200

    time intervals

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ $("#search-input input").keyup(function(event) {
     if (event.keyCode === 13) {
         var city = ($("#search-input input")).val();
 
-        cityVisualiserFunc(city);
+        cityVisualiserFunc(city);   
     }
 });
 
--- src/cityVisualise.js
+++ src/cityVisualise.js
@@ -3,24 +3,23 @@ function cityVisualiserFunc(city) {
     var forecastWeather = config.forecastWeatherApi + city + config.key;
 
     $.get(currentWeather)
-        .then(function(result) {
+        .then(function (result) {
             var table = $('<table>');
 
             var windDir = windDirectionProvider(result.wind.deg);
 
             if (windDir != null) {
-                table.append( '<tr><td>' + 'Wind' + '</td>' + '<td>' + result.wind.speed + ' m/s' + '<br>' + windDir  + '</td>' + '</tr>' )
-            }
-            else {
-                table.append( '<tr><td>' + 'Wind' + '</td>' + '<td>' + result.wind.speed + ' m/s' + '</td>' + '</tr>' );
+                table.append('<tr><td>' + 'Wind' + '</td>' + '<td>' + result.wind.speed + ' m/s' + '<br>' + windDir + '</td>' + '</tr>')
+            } else {
+                table.append('<tr><td>' + 'Wind' + '</td>' + '<td>' + result.wind.speed + ' m/s' + '</td>' + '</tr>');
             }
 
-            table.append( '<tr><td>' + 'Humidity' + '</td>' + '<td>' + result.main.humidity  + ' %' + '</td>' + '</tr>' )
-                .append( '<tr><td>' + 'Pressure' + '</td>' + '<td>' + result.main.pressure + ' hPa'  + '</td>' + '</tr>' )
-                .append( '<tr><td>' + 'Sunrise' + '</td>' + '<td>' +  unixToNormal(result.sys.sunrise) +  '</td>' + '</tr>' )
-                .append( '<tr><td>' + 'Sunset' + '</td>' + '<td>' + unixToNormal(result.sys.sunset) +  '</td>' + '</tr>' );
+            table.append('<tr><td>' + 'Humidity' + '</td>' + '<td>' + result.main.humidity + ' %' + '</td>' + '</tr>')
+                .append('<tr><td>' + 'Pressure' + '</td>' + '<td>' + result.main.pressure + ' hPa' + '</td>' + '</tr>')
+                .append('<tr><td>' + 'Sunrise' + '</td>' + '<td>' + unixToNormal(result.sys.sunrise) + '</td>' + '</tr>')
+                .append('<tr><td>' + 'Sunset' + '</td>' + '<td>' + unixToNormal(result.sys.sunset) + '</td>' + '</tr>');
 
-            var img = $('<img>').attr("src",`images/${result.weather[0].icon}.png`)[0];
+            var img = $('<img>').attr("src", `images/${result.weather[0].icon}.png`)[0];
 
             $('#result-current-weather').html($(`
             <div id="city-weather">
@@ -34,13 +33,22 @@ function cityVisualiserFunc(city) {
             </div>
             `));
         })
-        .catch(function(error) {
+        .catch(function (error) {
             alert('Invalid City!')
         });
 
     $.get(forecastWeather)
-        .then(function(result) {
-            var intervals = result.list.slice(0,8);
-            console.log(intervals);
+        .then(function (result) {
+            var intervals = result.list.slice(0, 8);
+
+            $('#result-forecast ul li').each(function (index) {
+                var interval = this;
+                var img = $('<img>').attr("src", `images/${intervals[index].weather[0].icon}.png`)[0];
+                
+                $(interval).html(`<h2>${unixToNormal(intervals[index].dt)}</h1>
+                                ${img.outerHTML}
+                                <p>${Math.round(intervals[index].main.temp - 273.15)} °C</p>
+                                <p>${intervals[index].weather[0].description}</p>`);
+            })
         });
 }
